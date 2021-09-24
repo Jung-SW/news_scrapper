0ee022807e8c59dc017d7808ec44412ea889a739
JungSW <jungsunwoong01@gmail.com>
2021-09-24 21:55:53 +0900

    joognang_news_scrapper

---
+++ news_scrapper.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+def search_news():
+    search = input()
+    URL = f"https://www.joongang.co.kr/search/news?keyword={search}"
+    return URL
+
+def extract_news(html):
+    news_title = html.find("a").get_text(strip=True).strip('\n')
+    news_url = html.find("a")['href']
+    return {
+        'news_title':news_title,
+        'news_url':news_url
+    }
+
+def extract_news(URL):
+    cards = []
+
+    res = requests.get(URL)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "lxml")
+    news = soup.find("ul", {"class":"story_list"}).find_all("h2", {"class":"headline"})
+    for new in news:
+        card = extract_news(new)
+        cards.append(card)
+    return cards
